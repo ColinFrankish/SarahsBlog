5ca748ff2ed6b1a401e869b4e84f63639cc2e5f3
Colin Frankish <colin.frankish@gmail.com>
2014-07-15 18:21:15 +0100

    first commit - rails,postgres,rspec,cucumber

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sarahsblog::Application.config.secret_key_base = '62ad6b7fe48e4970f598c97d525c8262c4a49f79c84f5b7d97fcea7cb5a8309f3df65a4110ea0f4736741af4a08ef91fb0d753806ab2e0db94dd847af0c5c13b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sarahsblog::Application.config.session_store :cookie_store, key: '_sarahsblog_session'
